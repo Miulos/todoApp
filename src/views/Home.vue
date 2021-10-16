7c57059e6426d6f09519ba631973670d8271f5a8
Miulos <1657096135@qq.com>
2021-10-16 18:54:33 +0800

    day1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <app-bar title="TODO" :left="bars" :right="search" />    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import AppBar from "../components/AppBar.vue"
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    AppBar
   },
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  color: white;
+}
+</style>
