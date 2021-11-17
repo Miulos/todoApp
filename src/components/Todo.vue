644729d1c989543724d7702d053b7fb8424fc3d8
Miulos <1657096135@qq.com>
2021-11-17 21:02:42 +0800

    *day4

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -13,13 +13,29 @@
         </span>
         <span :class="todo_progress_num">{{ progress }}</span>
       </div>
+      <div class="todo_tasks">
+        <h4 class="todo_subtitle" v-if="todayTasks.length">Today</h4>
+        <ul>
+          <li v-for="task in todayTasks" :key="task.id"><task :task="task"/></li>
+        </ul>
+        <h4 class="todo_subtitle" v-if="tomorrowTasks.length">Tomorrow</h4>
+        <ul>
+          <li v-for="task in tomorrowTasks" :key="task.id"><task :task="task"/></li>
+        </ul>
+        <h4 class="todo_subtitle" v-if="outdatedTasks.length">Outdated</h4>
+        <ul>
+          <li v-for="task in outdatedTasks" :key="task.id"><task :task="task"/></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import {today, tomorrow} from '../shared'
+import Task from './Task.vue'
 export default {
+  components: { Task },
   props: {
     todo: {
       type: Object,
@@ -32,11 +48,125 @@ export default {
   computed: {
     color () {
       return this.todo.colors[0]    // 时间08:14
+    },
+    progress () {
+      const totalCount = this.todo.tasks.filter(t => !t.deleted).length
+      const doneCount = this.todo.tasks.filter(t => !t.deleted && t.done).length
+      return '${Math.round((doneCount / totalCount) * 100)}%'
+    },
+    progressColor () {
+      const colorBottom = 'color-stop(30%, ${this.todo.colors[0]})'
+      const colorTop = 'to(${this.todo.colors[1]})'
+      return '-webkit-gradient(linear, left bottom, right bottom, ${colorBottom}, ${colorTop})'
+    },
+    todayTasks () {
+      return this.todo.tasks.filter(task => {
+        return task.date >= today && task.date < tomorrow
+      })
+    },
+    tomorrowTasks () {
+      return this.todo.tasks.filter(task => {
+        return task.date >= tomorrow
+      })
+    },
+    outdatedTasks () {
+      return this.todo.tasks.filter(task => {
+        return task.date < today
+      })
+    }
+  },
+  methods: {
+    handlleClick () {
+      const appRect = document.querySelector('#app').getBoundingClientRect()
+      const elRect = this.$el.getBoundingClientRect()
+      const todo = this.todo
+      const rect = {}
+      rect.top = elRect.top - appRect.top
+      rect.left = elRect.left - appRect.left
+      rect.whidth = elRect.width
+      rect.height = elRect.height
+      rect.appWidth = appRect.width
+      rect.appHeight = appRect.height
+      this.$emit('select', {rect, todo})
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.todo {
+  flex: 1;
+  margin: 0 8px;
+  overflow: hidden;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
+  color: #666;
+}
+.todo_selected {
+  visibility: hidden;
+}
+.tood-head {
+  display: flex;
+  padding: 20px;
+  height: 44px;
+  justify-content: space-between;
+  align-items: flex-start;
+  transform: matrix3d(0, 0, 0);
+  will-change: transform;
+}
+.todo_body {
+  padding: 0 20px;
+  transform: matrix3d(0, 189px, 0);
+  will-change: transform;
+}
+.todo_icon {
+  display: flex;
+  width: 44px;
+  height: 44px;
+  border: 1px solid #eee;
+  border-radius: 100%;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+}
+.todo_menu {
+  color: #eee;
+}
+.todo_tips {
+  opacity: 0.6;
+  font-size: 13px;
+  font-weight: 600;
+}
+.todo_title {
+  margin-top: 6px;
+  font-size: 32px;
+}
+.todo_progress {
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+}
+.todo_progress_line {
+  margin-right: 10px;
+  flex: 1;
+  height: 3px;
+  background-color: #eee;
 
+  i {
+    display: block;
+    height: 100%;
+    transition: all 0.3s ease;
+  }
+}
+.todo_progress_num {
+  font-size: 12px;
+}
+.todo_tasks {
+  opacity: 0;
+  transform: scale3d(1, 0, 1);
+}
+.todo_subtitle {
+  margin-top: 32px;
+  margin-bottom: 8px;
+  color: #999;
+}
 </style>
---
+++ src/components/TodoDetail.vue
@@ -0,0 +1,33 @@
+<template>
+  <transition name="show" @enter="handleEnter" @leaver="handleLeave">
+      <div class="todo-detail" v-if="sl=elected">
+          <app-bar @left="unselectTodo" />
+          <todo :todo="selected.todo" />
+      </div>
+  </transition>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+import AppBar from './AppBar.vue'
+import Todo from './Todo.vue'
+export default {
+  components: { AppBar, Todo },
+  computed: {
+    ...mapState(['selected', 'unselect'])
+  },
+  methods: {
+    ...mapMutations(['unselectedTod']),
+    handleEnter (el) {
+      Object.assign(el.style, {
+        top: '${this.selected.rect.top}px',
+        // 12:47
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
